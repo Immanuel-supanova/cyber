4fece6fa9cd6d92766acb7efbc8e8579dc5f0268
immanuel-supanova <njugunaimmanuel1@outlook.com>
2024-04-16 13:26:48 +0300

    user dataclasses

---
+++ schema/users.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel
+
+class UserBase(BaseModel):
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+class UserAuthenticate(UserCreate):
+    pass
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    is_admin: bool
+    is_staff: bool
+
+    class Config:
+        orm_mode = True
+
+class UserEmailChange(BaseModel):
+    old_email: str 
+    new_email: str
+
+class UserPasswordChange(UserBase):
+    old_password: str
+    new_password: str
+    confirm_password: str
+
+class UserIsActiveChange(UserBase):
+    is_active: bool
+
+class UserIsStaffChange(UserBase):
+    is_staff: bool
