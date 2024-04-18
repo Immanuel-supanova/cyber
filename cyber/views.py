aa80fd7a696778cbf462a9cbe3105289de105685
immanuel-supanova <njugunaimmanuel1@outlook.com>
2024-04-18 11:23:09 +0300

    Application token views are created

--- cyber/views.py
+++ cyber/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-# Create your views here.
+class ApplicationTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, app):
+        token = super().get_token(app)
+        return token
+
+class ApplicationTokenObtainPairView(TokenObtainPairView):
+    serializer_class = ApplicationTokenObtainPairSerializer
+
+class AplicationTokenRefreshView():
+    serializer_class = ApplicationTokenObtainPairSerializer
+    
