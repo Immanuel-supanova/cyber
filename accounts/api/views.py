5a7857eec0c30b4058e851e0d4c87a17ba3d1045
immanuel-supanova <njugunaimmanuel1@outlook.com>
2024-04-18 11:00:22 +0300

    made a few changes to the api views

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -12,6 +12,8 @@ User = get_user_model()
 class UserCreate(generics.CreateAPIView):
     serializer_class = RegisterSerializer
     queryset = User.objects.all()
+    permission_classes = (DjangoModelPermissions,)
+
 
 
 class UserList(generics.ListAPIView):
@@ -20,8 +22,8 @@ class UserList(generics.ListAPIView):
     """
 
     serializer_class = UserSerializer
-    queryset = User.objects.all()
-    permission_classes = (IsAuthenticated, )
+    queryset = User.objects.exclude(is_superuser=True)
+    permission_classes = (DjangoModelPermissions, )
 
 
 class UserRetrieve(generics.RetrieveAPIView):
@@ -30,13 +32,13 @@ class UserRetrieve(generics.RetrieveAPIView):
     """
 
     serializer_class = UserSerializer
-    queryset = User.objects.all()
-    permission_classes = (IsAuthenticated,)
+    queryset = User.objects.exclude(is_superuser=True)
+    permission_classes = (DjangoModelPermissions,)
 
 
 class CurrentUser(APIView):
     serializer_class = UserSerializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (DjangoModelPermissions,)
 
     def get(self, request):
         user = User.objects.get(id=request.user.id)
