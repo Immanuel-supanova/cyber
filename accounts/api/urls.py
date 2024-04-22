19bfc709533e1e41e712a0e9399d64a5c0236464
immanuel-supanova <njugunaimmanuel1@outlook.com>
2024-04-22 14:25:39 +0300

    Created community versions of password reset and password reset confirm views

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import UserList, UserRetrieve, UserCreate ,CurrentUser, UserPasswordReset
+from .views import UserList, UserPasswordResetConfirm, UserRetrieve, UserCreate ,CurrentUser, UserPasswordReset
 
 urlpatterns = [
     path('users/create/', UserCreate.as_view()),
@@ -8,5 +8,6 @@ urlpatterns = [
     path('users/<int:pk>/', UserRetrieve.as_view()),
     path('current-user/', CurrentUser.as_view()),
     path('users/password-reset/', UserPasswordReset.as_view()),
+    path('users/password-reset-confirm/', UserPasswordResetConfirm.as_view()),
 
 ]
